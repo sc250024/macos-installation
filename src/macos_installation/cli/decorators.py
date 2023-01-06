9dadfd97417dcdc7be7bb2b31ffb17845f53fcac
Scott Crooks <scott.crooks@gmail.com>
2023-01-06 11:22:08 +0100

    refactor(data): adding class for backup manifest
    The 'BackupManifest' dataclass provides a nicer way of dealing with the backup manifest that's placed inside of the ZIP file

--- src/macos_installation/cli/decorators.py
+++ src/macos_installation/cli/decorators.py
@@ -8,6 +8,14 @@ from macos_installation import config
 
 def common_backup_file(exists: bool = True) -> t.Callable:
     def inner_f(f):
+        """
+        The inner_f function is a decorator that takes the function f as an argument.
+        It then adds the click option -b/--backup-file to it, which is required and has a help message.
+        The inner_f function returns f with this added functionality.
+
+        :param f: Pass the function to be decorated
+        :return: The function f with the added option
+        """
         f = click.option(
             "-b",
             "--backup-file",
@@ -27,6 +35,17 @@ def common_password(
     prompt_required: bool = False,
     required: bool = False,
 ) -> t.Callable:
+    """
+    The common_password function is a decorator that adds the common password
+    option to a click command. The common_password option is used for decrypting
+    backup files and encrypting new backups. It has several options:
+
+    :param confirmation_prompt:bool=True: Determine if the user should be prompted for a confirmation of their password
+    :param prompt_required:bool=False: Determine if the password prompt should be shown
+    :param required:bool=False: Tell click that the parameter is not required
+    :return: A function that is decorated with the click
+    """
+
     def inner_f(f):
         f = click.option(
             "-p",
